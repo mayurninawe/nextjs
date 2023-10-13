92e1ff5899e3c5317de4112a98b03dd0bcb79633
mahesh240715 <mahesh.ninawe@digitas.com>
2023-10-13 14:00:28 +0530

    links for header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { createClient } from "@/prismicio"
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+
+export default async function Header (){
+    const client = createClient();
+    const settings = await client.getSingle("settings");
+    return 
+        <header>{settings.data.site_title}
+        <Link href="/">{settings.data.site_title}</Link>
+            <nav>
+                <ul>
+                    {settings.data.navigation.map(({link, label})=>(
+                        <li key={label}>
+                            <PrismicNextLink field={link}>Link</PrismicNextLink>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+} 
