bd576b4239a3ae7c6852bce907731a6b02c1dd75
mahesh240715 <mahesh.ninawe@digitas.com>
2023-10-13 18:16:00 +0530

    add page

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,11 +1,22 @@
-import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
+import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
-import { components } from "@/slices";
 
-export default function SliceSimulatorPage() {
-  return (
-    <SliceSimulator
-      sliceZone={(props) => <SliceZone {...props} components={components} />}
-    />
-  );
+import { createClient } from "@/prismicio";
+//import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+  return (<div className="text-red-500">Homepage</div>);
+  //return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
